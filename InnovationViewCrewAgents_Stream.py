c236d99697c1bdd5f278b0d83dee2748b0efe88b
Dev Bharti <devashish.bharti@converz.co.uk>
2024-09-26 17:11:21 +0100

    first

--- InnovationViewCrewAgents_Stream.py
+++ InnovationViewCrewAgents_Stream.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from crewai import Agent, Task, Crew, Process
 import os
 from dotenv import load_dotenv
